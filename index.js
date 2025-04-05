5a9bacddb17b737c512c61d39bbd7c8fddced2be
Ralpmike <ralphmike996@gmail.com>
2025-04-05 17:45:02 +0100

    Blocking and Non-blocking: Asynchronous nature of node js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const fs = require("fs");
+const fs = require("node:fs");
 
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+const textOut = `This is what we know about the avocado:${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 
+console.log(textIn, "This is the text in the file!");
 console.log("File written!");
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) =>
+//   setTimeout(() => {
+//     if (err) return console.log("Error reading file:", err);
+
+//     console.log("This is the data:", data);
+//   }, 3000)
+// );
+// console.log("Reading file...");
