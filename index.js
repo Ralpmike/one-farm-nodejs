21271d3be1aaff1a3437e5be685d175bfe22011f
Ralpmike <ralphmike996@gmail.com>
2025-04-06 19:51:57 +0100

    Blocking and Non-blocking: Asynchronous nature of node js

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const fs = require("node:fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+//? Blocking, synchronous way
 
-const textOut = `This is what we know about the avocado:${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-console.log(textIn, "This is the text in the file!");
-console.log("File written!");
+// const textOut = `This is what we know about the avocado:${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// console.log(textIn, "This is the text in the file!");
+// console.log("File written!");
+
+// ? Non-blocking, asynchronous way
+
+fs.readFile("./txt/start.txt4444", "utf-8", (err, data1) => {
+  if (err) return console.log("Error reading file:", err);
+
+  console.log("This is the data:", data1);
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log("This is the data:", data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log("This is the data:", data3);
+      fs.writeFile(`./txt/final.txt`, `${data2} \n${data3}`, "utf-8", (err) => {
+        console.log("file has been written!");
+      });
+    });
+  });
+});
+
+console.log("Reading file...");
 
 // fs.readFile("./txt/start.txt", "utf-8", (err, data) =>
 //   setTimeout(() => {
