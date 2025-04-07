545148ad216918391eca47f9d6f2b081840de472
Ralpmike <ralphmike996@gmail.com>
2025-04-07 11:54:37 +0100

    Creating a simple web server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require("node:fs");
+const http = require("node:http");
+
+////////////////////////////////////////////////////////
+
+// FILE SYSTEM MODULE
 
 //? Blocking, synchronous way
 
@@ -12,22 +17,22 @@ const fs = require("node:fs");
 
 // ? Non-blocking, asynchronous way
 
-fs.readFile("./txt/start.txt4444", "utf-8", (err, data1) => {
-  if (err) return console.log("Error reading file:", err);
+// fs.readFile("./txt/start.txt4444", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error reading file:", err);
 
-  console.log("This is the data:", data1);
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log("This is the data:", data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log("This is the data:", data3);
-      fs.writeFile(`./txt/final.txt`, `${data2} \n${data3}`, "utf-8", (err) => {
-        console.log("file has been written!");
-      });
-    });
-  });
-});
+//   console.log("This is the data:", data1);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log("This is the data:", data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log("This is the data:", data3);
+//       fs.writeFile(`./txt/final.txt`, `${data2} \n${data3}`, "utf-8", (err) => {
+//         console.log("file has been written!");
+//       });
+//     });
+//   });
+// });
 
-console.log("Reading file...");
+// console.log("Reading file...");
 
 // fs.readFile("./txt/start.txt", "utf-8", (err, data) =>
 //   setTimeout(() => {
@@ -37,3 +42,12 @@ console.log("Reading file...");
 //   }, 3000)
 // );
 // console.log("Reading file...");
+
+// !SERVER MODULE
+
+const server = http.createServer((req, res) => {
+  console.log(req), res.end("Hello from the server!");
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("server running on port 8000");
+});
