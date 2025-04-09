f819cf5c65af8d7655f554bd33d27553dda708ae
Ralpmike <ralphmike996@gmail.com>
2025-04-09 14:36:03 +0100

    Modules 2: using our own modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("node:fs");
 const http = require("node:http");
 const url = require("node:url");
-
+const replaceTemplate = require("./modules/replaceTemplate");
 ////////////////////////////////////////////////////////
 
 // FILE SYSTEM MODULE
@@ -43,22 +43,6 @@ const url = require("node:url");
 //   }, 3000)
 // );
 // console.log("Reading file...");
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-};
 
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
